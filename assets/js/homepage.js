4ae291a7d961109d20a33b4e9b06e84885e039b3
techreative <nathanielejones@hotmail.com>
2021-08-23 12:50:53 -0400

    added displays

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -8,9 +8,9 @@ var getUserRepos = function(user) {
   // make a request to the url
   fetch(apiUrl).then(function(response) {
     if (response.ok) {
-      response.json().then(function(data) {
+        response.json().then(function(data) {
         displayRepos(data, user);
-      });
+        });
     } 
     else {
       alert("Error: GitHub User Not Found");
